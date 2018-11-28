654221afb9af5101532ffa46abe3671e16a0e969
Mikolaj Figurski <meek.f80@gmail.com>
2018-11-28 14:45:20 -0500

    Added async sql query function

--- back/db.js
+++ back/db.js
@@ -11,15 +11,26 @@ class Database {
   }
 
   test() {
-    this.connection.connect();
+    this.query("SELECT 1+1 AS solution")
+      .then(rows => {
+        console.log(rows);
+      });
+  }
 
-    this.connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-      if (err) throw err;
-      console.log("DB online?", rows[0].solution == 2);
-      console.log(rows);
+  /**
+   * async function to execute SQL queries
+   * @param sql code to execute
+   * @return promise, with resolve getting rows
+   */
+  query(sql) {
+    return new Promise((resolve, reject) => {
+      this.connection.connect();
+      this.connection.query(sql, (err, rows) => { //when results come back...
+        if (err) return reject(err);// throw all errors
+        this.connection.end();      // end connection
+        resolve(rows);              // resolve promise with rows
+      });
     });
-
-    this.connection.end();
   }
 }
 
