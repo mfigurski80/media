c3ec564b2fe1c41fed736b19e4a97fc55bb8771e
Mikolaj Figurski <meek.f80@gmail.com>
2019-02-07 12:21:27 -0500

    Set up basic react-router structure

--- front/src/views/Home.js
+++ front/src/views/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -18,6 +18,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <h1>Home</h1>
         <p>Posts:</p>
         <ul>
           {this.state.posts.map((post) =>
